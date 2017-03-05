3717426e76ca3abe26dde2067e36d956a96cef6c
Syunpei Nagata <n1510124@edu.cc.uec.ac.jp>
2017-03-05 20:22:15 +0900

    基本の画面遷移を作成

---
+++ TabbedFC/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedFC
+//
+//  Created by 永田駿平 on 2017/03/05.
+//  Copyright © 2017年 feelingCouplePBL. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
